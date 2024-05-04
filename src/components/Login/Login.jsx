e4f87739dd17c3fc342ceaf29ea193ef0d6747c1
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-05-04 19:03:01 -0300

    Practica Obligatoria 3

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+
+const Login = () => {
+
+    const [username, setUsername] = useState("");
+
+    const setUsernameHandler = (event) => {
+        setUsername(event.target.value);
+        username.toLowerCase().includes("o") ? alert("Por favor, ¡Nombres de usuario sin letra 'o'!") : 0;
+    }
+
+    const submitUsernameHandler = (event) => {
+        event.preventDefault();
+        username.toLowerCase().includes("o") || username.trim() == ""
+        ? alert("Usuario inválido para registrarse.")
+        : alert("Usuario registrado correctamente.");
+
+        setUsername("");
+    }
+
+    return(
+        <>
+            <label id="username">Nombre de usuario: </label>
+            <input id="username" type="text" onChange={setUsernameHandler} value={username}></input>
+            <button id="register-btn" type="submit" onClick={submitUsernameHandler}>Registrarse</button>
+            <p>{username}</p>
+        </>
+    );
+};
+
+export default Login;
